f820ce6c5f442e2ce254cc3ae516b731ad610eca
AjwelkarNilesh <nilesh.ajwelkar@gmail.com>
2018-12-07 13:07:45 +0530

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,10 +17,10 @@ class TweetsController < ApplicationController
   private
     def create_client
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key = "rbvbAxpHID3kMspenWWow"
-      config.consumer_secret = "eKq2e3Sv7DZFEvVKQwecbRC1q5QiTG2E5HNuyk67A"
-      config.access_token = "89089461-jBAqMjYgFXLf7IBs3tvYNR4kAhvJlxFHCysdAcGmd"
-      config.access_token_secret = "QTlsbDOprs3ZLssBpCMeL0bmlofDZc76eh5CvQUXHqogZ"
+      config.consumer_key = ""
+      config.consumer_secret = ""
+      config.access_token = ""
+      config.access_token_secret = ""
     end
   end
 end
